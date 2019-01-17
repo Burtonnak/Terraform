fa7cc239204f6165f678a41a4315c88123b62e8d
Burtonnak <cedricporte@iMac-de-Cedric.local>
2019-01-17 11:33:22 +0100

    add ignore file and mysqk http default conf

--- Stage/Webservices/aws/user-data.sh
+++ Stage/Webservices/aws/user-data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo yum -y install httpd strace sysstat mlocate
+sudo yum -y install httpd strace sysstat mlocate mod_ssl
 		
 cat > /var/www/html/index.html <<EOF
 <h1>Hello, Auriel, Alexiel and Lisa</h1>
@@ -8,7 +8,13 @@ cat > /var/www/html/index.html <<EOF
 <p>DB Port : ${db_port}</p>
 EOF
 
-sudo apachectl start
+sudo systemctl start httpd
+sudo systemctl enable httpd
 sudo yum -y update
 sudo updatedb
+sudo usermod -a -G apache ec2-user
+sudo chown -R ec2-user:apache /var/www
+sudo chmod 2775 /var/www && find /var/www -type d -exec sudo chmod 2775 {} \;
+find /var/www -type f -exec sudo chmod 0664 {} \;
+
 
